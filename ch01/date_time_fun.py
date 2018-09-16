12078b152dd3cd4c9620c388dc593c72862ba071
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-09-16 18:55:38 -0400

    time after time for Cyndi Lauper

--- ch01/date_time_fun.py
+++ ch01/date_time_fun.py
@@ -1,4 +1,7 @@
 import datetime
-
+import time
 my_date = [str(datetime.date.today().year), str(datetime.date.today().month), str(datetime.date.today().day)]
 print("".join(my_date))
+
+print(datetime.date.isoformat(datetime.date.today()))
+print(time.strftime('%Y%m%d %I:%M%p'))
