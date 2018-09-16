41a837adbed8c962ae2972355db82ac74a43335d
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-09-16 16:27:48 -0400

    pop goes the weasel on a clear day

--- ch02/pop_goes_the_weasel.py
+++ ch02/pop_goes_the_weasel.py
@@ -1,5 +1,8 @@
 my_list = ['pop', 'goes', 'the', 'weasel']
-
+first_list = ['on', 'a']
+second_list = ['clear', 'day']
+first_list.extend(second_list)
 print(my_list)
 my_list.pop()
 print(my_list)
+print(first_list)
