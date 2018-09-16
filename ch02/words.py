dda8070060bb47c6a3d176397bf5c8584793053e
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-09-16 15:36:04 -0400

    more list stuff in HFPy

--- ch02/words.py
+++ ch02/words.py
@@ -1,6 +1,12 @@
 words = ['Bonjour', 'tout', 'le', 'monde']
 mixed_stuff = ['apple', 1, 42.0, True]
-print(words)
+list_of_Lists = [words, mixed_stuff]
+
 
 for item in mixed_stuff:
+    print(type(item))
+
+print()
+
+for item in  list_of_Lists:
     print(type(item))
