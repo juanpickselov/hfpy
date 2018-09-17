df8fd171aeaa580ad02aeeb0abef49f654c61b2d
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-09-16 20:36:39 -0400

    more list fun

---
+++ ch02/first_second_third.py
@@ -0,0 +1,8 @@
+first = [1, 2, 3, 4, 5]
+second = first
+print(first)
+print(second)
+second.append(6)
+print(first)
+print(second)
+print('Note how first and second are pointing to the same list!')
